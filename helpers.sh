6441321bb6810615df3ceeb6cbf39ee696a788e0
Tatsinnit <tamishra@microsoft.com>
2021-11-21 19:13:28 +1300

    swtich to kubernetes mixim.

--- helpers.sh
+++ helpers.sh
@@ -3,19 +3,14 @@ set -euo pipefail
 
 install() {
   echo "Install Azure Service Oeprator"
-  echo "Install CRDs"
-  kubectl apply -f 'https://github.com/operator-framework/operator-lifecycle-manager/releases/download/v0.18.3/crds.yaml'
-  echo "Install OLM"
-  kubectl apply -f 'https://github.com/operator-framework/operator-lifecycle-manager/releases/download/v0.18.3/olm.yaml'
-  echo "OLM installation complete"
 }
 
 upgrade() {
-  echo No upgrade feature available
+  echo "No upgrade feature available"
 }
 
 uninstall() {
-  echo No uninstall feature available
+  echo "No uninstall feature available"
 }
 
 # Call the requested function and pass the arguments as-is
