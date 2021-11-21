023aa1ad155858dabac16c4117f56ae63f36160d
Tatsinnit <tamishra@microsoft.com>
2021-11-21 19:28:27 +1300

    weird kubernetes mixim behaviour, trying install script way again.

--- helpers.sh
+++ helpers.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-install() {
+install-olm() {
   echo "Install Azure Service Oeprator"
+  echo "Install CRDs"
+  kubectl apply -f 'https://github.com/operator-framework/operator-lifecycle-manager/releases/download/v0.18.3/crds.yaml'
+  echo "Install OLM"
+  kubectl apply -f 'https://github.com/operator-framework/operator-lifecycle-manager/releases/download/v0.18.3/olm.yaml'
+  echo "OLM installation complete"
 }
 
 upgrade() {
